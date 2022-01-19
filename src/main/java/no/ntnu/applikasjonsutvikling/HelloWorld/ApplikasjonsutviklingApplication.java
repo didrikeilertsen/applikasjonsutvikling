699bae36f1869e57b213fbeeba4b3fd8f845e1e7
Didrik Eilertsen <didrik.eilertsen@gmail.com>
2022-01-19 16:01:26 +0100

    initial commit

---
+++ src/main/java/no/ntnu/applikasjonsutvikling/HelloWorld/ApplikasjonsutviklingApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.applikasjonsutvikling.HelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplikasjonsutviklingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplikasjonsutviklingApplication.class, args);
+	}
+
+}
